570916a064679505009fc12a95c59d73fe418239
Victor Benichoux <victorbenichoux@users.noreply.github.com>
2021-10-21 09:34:03 +0200

    stdrenderer: add emoji, refactor single clock print

--- ticktock/std.py
+++ ticktock/std.py
@@ -6,12 +6,17 @@ from ticktock.utils import format_ns_interval
 
 class StandardRenderer:
     def render(self, render_data: Dict[str, Dict[str, AggregateTimes]]):
-        print(f"# {len(render_data)} clocks")
+        ls = []
         for clock_name, clock_data in render_data.items():
-            for tick_key, times in clock_data.items():
-                print(
-                    f"{clock_name} [{tick_key}] "
-                    f"avg = {format_ns_interval(times.avg_time_ns)}, "
-                    f"last = {format_ns_interval(times.last_time_ns())}, "
-                    f"n = {times.n_periods}"
-                )
+            for line in StandardRenderer.render_clock(clock_name, clock_data):
+                ls.append(line)
+        print("\n".join(ls))
+
+    def render_clock(clock_name, clock_data: Dict[str, AggregateTimes]):
+        for tick_key, times in clock_data.items():
+            yield (
+                f"⏱️ {clock_name} [{tick_key}] "
+                f"avg = {format_ns_interval(times.avg_time_ns)}, "
+                f"last = {format_ns_interval(times.last_time_ns())}, "
+                f"n = {times.n_periods}"
+            )
