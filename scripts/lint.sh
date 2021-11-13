eeee7523f1c1cac171bc80c353075629b1f72455
Victor Benichoux <victor.benichoux@getmayday.co>
2021-11-13 16:17:07 +0100

    scripts: add apply as argument to lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ $1 == apply ]] ; then
+    black ticktock tests
+    isort ticktock tests
+fi
+
 black ticktock tests --check
 isort ticktock tests --check-only
 flake8 ticktock tests 
