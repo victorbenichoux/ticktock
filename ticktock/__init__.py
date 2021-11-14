2828616e52db00d5e9dc25ec3c02575764ca7995
Victor Benichoux <victor.benichoux@getmayday.co>
2021-11-14 20:03:55 +0100

    set_format: expose at the package level

--- ticktock/__init__.py
+++ ticktock/__init__.py
@@ -1,3 +1,3 @@
-from ticktock.timer import disable, enable, tick, ticktock  # noqa: F401
+from ticktock.timer import disable, enable, set_format, tick, ticktock  # noqa: F401
 
 __version__ = "0.0.6"
