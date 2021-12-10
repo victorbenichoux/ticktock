cb9beda631567d17cc94f762a6fed8a44067d918
Victor Benichoux <victor.benichoux@getmayday.co>
2021-12-10 10:28:42 +0100

    Bump version: 0.1.0 → 0.1.1

--- ticktock/__init__.py
+++ ticktock/__init__.py
@@ -3,4 +3,4 @@ from ticktock.clocks import tick
 from ticktock.collection import disable, enable, set_format, set_period
 from ticktock.timers import ticktock
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
